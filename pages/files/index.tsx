1051c41950ab3958569dbac51e791ad7199724c3
eltonninja <eltonninja411@gmail.com>
2023-08-07 06:15:25 -0700

    feat: add sort type control to ui

--- pages/files/index.tsx
+++ pages/files/index.tsx
@@ -2,11 +2,16 @@
 
 import { FileCard } from "@/components/FileCard";
 import { File } from "@/types/File";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 
 export default function Files() {
   const [files, setFiles] = useState<File[]>([]);
   const [busy, setBusy] = useState<boolean>(false);
+  const [sortType, setSortType] = useState<SortType>(SortType.CreatedAt);
+
+  const handleSortTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortType(e.target.value as SortType);
+  };
 
   useEffect(() => {
     setBusy(true);
@@ -32,6 +37,11 @@ export default function Files() {
 
   return (
     <div>
+      <select value={sortType} onChange={handleSortTypeChange}>
+        <option value={SortType.CreatedAt}>Created At</option>
+        <option value={SortType.NameAscending}>a - z</option>
+        <option value={SortType.NameDescending}>z - a</option>
+      </select>
       <ul className="grid grid-cols-2 gap-4">
         {files.map((file) => (
           <li key={file.name}>
---
+++ types/SortType.ts
@@ -0,0 +1,5 @@
+enum SortType {
+  CreatedAt = "CreatedAt",
+  NameAscending = "NameAscending",
+  NameDescending = "NameDescending",
+}
